b1257fa68f20c8aded1318accf91c189aa12e9fd
Andre <andrewilliamsyo@gmail.com>
2016-01-16 23:00:07 -0800

    cleanup and improved readability

--- week1/index.js
+++ week1/index.js
@@ -1,35 +1,29 @@
 var request = require('request');
 var _ = require('underscore');
+var moment = require('moment');
+var config = require('./lib/config.js');
 
 
-
-request('https://api.instagram.com/v1/tags/goodeggseats/media/recent?access_token=TOKEN', function (error, response, body) {
+request('https://api.instagram.com/v1/tags/goodeggseats/media/recent?access_token=' + config.instagram.token, function (error, response, body) {
   var newBody = JSON.parse(body)
 
   if (error) {
-    console.log("Error: " + error)
+    console.error("Error: " + error);
+    process.exit(1);
   }
-  if (response.statusCode !== 200){
+  else if (response.statusCode !== 200) {
     return console.log('Invalid Status Code Returned:', response.statusCode);
   }
   else {
-      _.forEach(newBody, function(arr){
-        _.forEach(arr, function(user){
-          if (user.caption === undefined){
+      _.forEach(newBody, function(arr) {
+        _.forEach(arr, function(obj) {
+          if (obj.caption === undefined) {
             return null;
-          } else{
-            var a = new Date(user.caption['created_time'] * 1000)
-            var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-            var year = a.getFullYear();
-            var month = months[a.getMonth()];
-            var date = a.getDate();
-            var hour = a.getHours();
-            var min = a.getMinutes();
-            var sec = a.getSeconds();
-            var time = month + ' ' + date + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-            console.log("Date : " + time + " User: " + user.caption['from']['username'] + " Text: " + user.caption['text'] + "\n" )
+          } else {
+            var time = moment.unix(obj.caption['created_time']).format("YYYY-MM-DD hh:mm a")
+            return console.log("Date: " + time + "\nUser: " + obj.caption['from']['username'] + "\nText: " + obj.caption['text'] + "\n" );
           }
-        })
-      })
+        });
+      });
   }
-})
+});
