8f59cbded59aca13499c25a2f88483161c79c2ae
Andre <andrewilliamsyo@gmail.com>
2016-02-02 11:58:06 -0800

    added fibrous

--- week1/index.js
+++ week1/index.js
@@ -1,36 +1,24 @@
 var request = require('request');
+var fibrous = require ('fibrous');
 var _ = require('underscore');
-var moment = require('moment');
 var config = require('../lib/config.js');
 var mu = require('mu2');
 
-mu.root = __dirname;
 
-request('https://api.instagram.com/v1/tags/goodeggseats/media/recent?access_token=' + config.instagram.token, function (error, response, body) {
-  var newBody = JSON.parse(body);
+module.exports = {
+    instagram: function() {
 
-  if (error) {
-    console.error("Error: " + error);
-    process.exit(1);
+      var resp = request.sync('https://api.instagram.com/v1/tags/goodeggseats/media/recent?access_token=' + config.instagram.token);
+
+      if (resp.statusCode !== 200) {
+        return console.log('Invalid Status Code Returned:', resp.statusCode);
+      }
+      else {
+        var newBody = JSON.parse(resp.body);
+        var newArr = _.map(newBody['data'], function(arr){
+          return arr['images']['thumbnail'];
+        })
+        return newArr;
+    }
   }
-  else if (response.statusCode !== 200) {
-    return console.log('Invalid Status Code Returned:', response.statusCode);
-  }
-  else {
-      _.forEach(newBody, function(arr) {
-        _.forEach(arr, function(obj) {
-          if (obj.caption === undefined) {
-            return null;
-          } else {
-            var time = moment.unix(obj.caption['created_time']).format("YYYY-MM-DD hh:mm a");
-            
-            mu.compileAndRender('template.mustache', {img: obj['images']['thumbnail']['url']})
-              .on('data', function (data) {
-                console.log(data.toString());
-              });
-            // return console.log("Date: " + time + "\nUser: " + obj.caption['from']['username'] + "\nText: " + obj.caption['text'] + "\n" );
-          }
-        });
-      });
-  }
-});
+}
